4f0d8c5c93b868dbce8fc25886131c1d60907086
Igor Bohn <igorfbohn@gmail.com>
2021-04-27 18:06:16 -0300

    Adicionado ThemeSwitcher e TodoList como novos components

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styuled from 'styled-components';
+
+const  TodoList = () => (
+    <ul>
+        <li>Fazer café</li>
+        <li>Estudar</li>
+        <li>Praticar</li>
+    </ul>
+)
+
+export default TodoList;
